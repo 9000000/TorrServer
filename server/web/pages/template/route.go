489821fd8ce3302ccc543629339b29e721c65ad0
9000000 <hai90000001@gmail.com>
2026-01-02 12:01:46 +0700

    feat: Add new web UI template with embedded static assets and routing.

--- server/web/pages/template/route.go
+++ server/web/pages/template/route.go
@@ -301,18 +301,18 @@ func RouteWebPages(route gin.IRouter) {
 		c.Data(200, "application/json", Staticjs2f410fe75chunkjsmap)
 	})
 
-	route.GET("/static/js/main.c6c13838.chunk.js", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmainc6c13838chunkjs))
+	route.GET("/static/js/main.7160e566.chunk.js", func(c *gin.Context) {
+		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmain7160e566chunkjs))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "text/javascript; charset=utf-8", Staticjsmainc6c13838chunkjs)
+		c.Data(200, "text/javascript; charset=utf-8", Staticjsmain7160e566chunkjs)
 	})
 
-	route.GET("/static/js/main.c6c13838.chunk.js.map", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmainc6c13838chunkjsmap))
+	route.GET("/static/js/main.7160e566.chunk.js.map", func(c *gin.Context) {
+		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmain7160e566chunkjsmap))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "application/json", Staticjsmainc6c13838chunkjsmap)
+		c.Data(200, "application/json", Staticjsmain7160e566chunkjsmap)
 	})
 
 	route.GET("/static/js/runtime-main.5ed86a79.js", func(c *gin.Context) {
