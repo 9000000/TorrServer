4ce54e3700735e12523032008d4c722e88da3397
9000000 <hai90000001@gmail.com>
2026-04-02 11:50:39 +0700

    fix

--- docker/lite/makedocker.sh
+++ docker/lite/makedocker.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # Build from project root to include source code
+
+# Ensure a buildx builder with multi-platform support is used
+BUILDER_NAME="torr-builder"
+if ! docker buildx inspect "$BUILDER_NAME" > /dev/null 2>&1; then
+  echo "Creating new buildx builder '$BUILDER_NAME' for multi-platform support..."
+  docker buildx create --name "$BUILDER_NAME" --driver docker-container --use
+else
+  docker buildx use "$BUILDER_NAME"
+fi
+
 docker buildx build -f Dockerfile --platform "linux/386,linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6" --tag 9000000/torrserver:lite --push ../../
