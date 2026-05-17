1dbee5558f31ee6265d7510dd629cf7dbf58317a
9000000 <hai90000001@gmail.com>
2026-05-17 22:35:05 +0700

    upversion

--- server/version/version.go
+++ server/version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is set at build time via -ldflags "-X server/version.Version=<tag>"
-var Version = "MatriX.141.8"
+var Version = "MatriX.141.9"
 
 func GetTorrentVersion() string {
 	bi, ok := debug.ReadBuildInfo()
