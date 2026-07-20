e84f8c8a5e0709a6ca2327e848faa792ad82d228
9000000 <hai90000001@gmail.com>
2026-07-20 11:25:10 +0700

    up

--- nix/packages/torrserver.nix
+++ nix/packages/torrserver.nix
@@ -9,7 +9,7 @@
 }:
 pkgs.stdenv.mkDerivation rec {
   pname = "torrserver";
-  version = "MatriX.142.3";
+  version = "MatriX.142.4";
 
   src = pkgs.fetchgit {
     url = "https://github.com/YouROK/TorrServer.git";
